90cffa8745ee5075d4026c79c862bfdda15962c0
iLorenzz <ldutra0912@gmail.com>
2024-07-23 11:26:51 -0300

    table atributtes adjustment. It need to be not null

--- sql/post.sql
+++ sql/post.sql
@@ -2,9 +2,11 @@
 
 use secretVerse;
 
+drop table if exists post;
+
 create table post(
     postId int auto_increment primary key,
-    tittle varchar(128),
-    confession varchar(1024),
+    tittle varchar(128) not null,
+    confession varchar(1024) not null,
     created_in timestamp default current_timestamp()
 ) engine=innodb;
